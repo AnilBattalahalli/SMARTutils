7d24e265bedbd4f16f88914104adc760fefc0c8d
Anil Battalahalli <anilbattalahalli@automata-2.local>
2022-03-26 15:03:57 -0400

    Fixed the functions

--- R/nocovariates.R
+++ R/nocovariates.R
@@ -2,116 +2,6 @@
 #' @importFrom utils combn
 NULL
 
-#' @title Function to get treatment recipe from the conditionals
-#'
-#' @description Does what it says
-#' @param cell_mu a vector of conditional means
-#' @param cell_var a vector of conditional vars
-#' @param cell_cor a vector of conditional correlation
-#' @param N number of clusters
-#' @param m number of participants in a cluster
-#' @param p_1 probability of response of A_1 = 1
-#' @param p_2 probability of response of A_1 = -1
-#' @export nocovairiates.from_conditional
-nocovairiates.from_conditional <- function(cell_mu = c(10,8,2,4,-2,3), cell_var = c(100,100,100,100,100,100),
-                                           cell_cor = c(0.1, 0.2, 0.15, 0.12, 0.11, 0.07), N=200, m=5, p_1=0.3, p_2=0.2){
-
-  if (length(m) != N){
-    m <- rep(m[1], N)
-  }
-
-  p_A1 <- 0.5
-  p_A2 <- 0.5
-
-  q_1 <-  1 - p_1
-  q_2 <-  1 - p_2
-
-  cell_cov <-  cell_var*cell_cor
-
-  treat_mu <-  c((cell_mu[1]*p_1) + (cell_mu[2]*q_1), (cell_mu[1]*p_1) + (cell_mu[3]*q_1),
-                 (cell_mu[4]*p_2) + (cell_mu[5]*q_2), (cell_mu[4]*p_2) + (cell_mu[6]*q_2))
-
-  treat_var <-  c((cell_var[1]*p_1 + cell_var[2]*q_1), (cell_var[1]*p_1 + cell_var[3]*q_1),
-                (cell_var[4]*p_2 + cell_var[5]*q_2), (cell_var[4]*p_2 + cell_var[6]*q_2))
-
-  mean_diff_term <-  c(((cell_mu[1]-cell_mu[2])^2)*p_1*q_1, ((cell_mu[1]-cell_mu[3])^2)*p_1*q_1,
-                     ((cell_mu[4]-cell_mu[5])^2)*p_2*q_2, ((cell_mu[4]-cell_mu[6])^2)*p_2*q_2)
-
-  treat_var <-  treat_var + mean_diff_term
-
-  treat_cov <-  c((cell_cov[1]*p_1) + (cell_cov[2]*q_1), (cell_cov[1]*p_1) + (cell_cov[3]*q_1),
-                (cell_cov[4]*p_2) + (cell_cov[5]*q_2), (cell_cov[4]*p_2 + cell_cov[6]*q_2))
-
-  treat_cov <-  treat_cov + mean_diff_term
-  treat_cor <-  treat_cov/treat_var
-  A <-  matrix(c(1,1,1,1,1,1,-1,-1,1,-1,1,-1,1,-1,-1,1), 4, 4)
-  B <-  matrix(treat_mu, 4, 1)
-  betas <- solve(A, B)
-
-  treat_summary <-  data.frame(treat_mu, treat_var, treat_cor, betas)
-  row.names(treat_summary) <-  c("(1, 1)", "(1, -1)", "(-1, 1)", "(-1,-1)" )
-
-  recipe <- list(cell_mu=cell_mu, cell_var=cell_var, cell_cor=cell_cor, cell_cov=cell_cov,
-                 treat_mu=treat_mu, treat_var=treat_var, treat_cov=treat_cov, treat_cor=treat_cor,
-                 p_1=p_1, p_2=p_2, p_A1=p_A1, p_A2=p_A2, N=N, m=m, betas=betas, treat_summary=treat_summary)
-  return(recipe)
-}
-
-#' @title Function to generate data from treatment
-#' @param recipe the generated recipe from nocovairiates.from_conditional()
-#' @export nocovariates.treat
-nocovariates.treat <- function(recipe){
-  encoder <- list("1,1,."=1, "1,0,1"=2, "1,0,-1"=3, "-1,1,."=4, "-1,0,1"=5, "-1,0,-1"=6)
-  N <- recipe$N
-  m_vec <- recipe$m
-  cell_mu <- recipe$cell_mu
-  cell_var <- recipe$cell_var
-  cell_cor <- recipe$cell_cor
-  cell_cov <- recipe$cell_cov
-  p_1 <- recipe$p_1
-  p_2 <- recipe$p_2
-  p_A1 <- recipe$p_A1
-  p_A2 <- recipe$p_A2
-
-  data <- matrix(nrow=0, ncol=6)
-
-  for (clusID in 1:N){
-    m <- m_vec[clusID]
-    A1 <-  (2*rbinom(1, 1, p_A1)) - 1
-
-    if (A1 == 1){
-      R <-  rbinom(1,1,p_1)
-      if (R == 1){
-        treat_code <- "1,1,."
-        A2 <- 1
-      } else if (R == 0){
-        A2 <-  (2*rbinom(1, 1, p_A2)) - 1
-        treat_code <- sprintf("1,0,%d", A2)
-      }
-    } else if (A1 == -1){
-      R <-  rbinom(1, 1 ,p_1)
-      if (R == 1){
-        treat_code <- "-1,1,."
-        A2 <- 1
-      }
-      else if (R == 0){
-        A2 <-  (2*rbinom(1, 1, p_A2)) - 1
-        treat_code <- sprintf("-1,0,%d", A2)
-      }
-    }
-
-    cell_index <- as.numeric(encoder[treat_code])
-    V <-  cell_var[cell_index] * (diag(m)+(cell_cor[cell_index]*(matrix(1,m,m)-diag(m))))
-    Y <- t(cell_mu[cell_index]+mvtnorm::rmvnorm(1, rep(0,m), V, method='chol'))
-
-    tempmat <- cbind(rep(clusID, m), seq(1,m), rep(A1, m), rep(R, m), rep(A2, m), Y)
-    data <- rbind(data, tempmat)
-  }
-  full_data_frame = as.data.frame(data)
-  names(full_data_frame) = c("i", "j", "A1", "R", "A2", "Y")
-  return(full_data_frame)
-}
-
 rep.row<-function(x,n){
   matrix(rep(x,each=n),nrow=n)
 }
@@ -167,15 +57,17 @@ get_varrho_star <- function(data, epsilon){
     a2 <- dtrenc[[d]][2]
     s <- 0
     den <- 0
-    for (i in 1:N){
-      data_sub <- data[data$i == i, ]
+    for (ci in 1:N){
+      data_sub <- data[data$i == ci, ]
       ni <- nrow(data_sub)
       A1 <- data_sub$A1[1]
       R <- data_sub$R[1]
       A2 <- data_sub$A2[1]
-      eps_sq <- epsilon[[d]][[i]]^2
-      s <- s + (IWi(A1, R, A2, a1, a2) * sum(eps_sq))
-      den <- den + (IWi(A1, R, A2, a1, a2) * ni)
+      eps_sq <- epsilon[[d]][[ci]]^2
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        s <- s + (IWi(A1, R, A2, a1, a2) * sum(eps_sq))
+        den <- den + (IWi(A1, R, A2, a1, a2) * ni)
+      }
     }
     var_star[d] <- s/den
   }
@@ -185,15 +77,17 @@ get_varrho_star <- function(data, epsilon){
     a2 <- dtrenc[[d]][2]
     s <- 0
     den <- 0
-    for (i in 1:N){
-      data_sub <- data[data$i == i, ]
+    for (ci in 1:N){
+      data_sub <- data[data$i == ci, ]
       ni <- nrow(data_sub)
       A1 <- data_sub$A1[1]
       R <- data_sub$R[1]
       A2 <- data_sub$A2[1]
-      eps_mul_sum <- perm_mul_sum(epsilon[[d]][[i]])
-      s <- s + (IWi(A1, R, A2, a1, a2) * eps_mul_sum)
-      den <- den + (IWi(A1, R, A2, a1, a2) * ni * (ni - 1))
+      eps_mul_sum <- perm_mul_sum(epsilon[[d]][[ci]])
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        s <- s + (IWi(A1, R, A2, a1, a2) * eps_mul_sum)
+        den <- den + (IWi(A1, R, A2, a1, a2) * ni * (ni - 1))
+      }
     }
     den <- den * var_star[[d]]
     rho_star[d] <- s/den
@@ -216,8 +110,10 @@ get_beta <- function(var, rho, data){
     for (d in dtrs){
       a1 <- dtrenc[[d]][1]
       a2 <- dtrenc[[d]][2]
-      V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
-      f = f + (IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% D(ni, a1, a2))
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
+        f = f + (IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% D(ni, a1, a2))
+      }
     }
   }
 
@@ -232,18 +128,195 @@ get_beta <- function(var, rho, data){
     for (d in dtrs){
       a1 <- dtrenc[[d]][1]
       a2 <- dtrenc[[d]][2]
-      V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
-      s <- s + (IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% Y)
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
+        s <- s + (IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% Y)
+      }
     }
   }
   beta_hat <- solve(f) %*% s
   return(beta_hat)
 }
 
-#' @title Function to estimate betas given the data, variance and correlation
-#' @param data data with cluster ID, A1, R, A2, Y
-#' @export nocovariates.estimate_betas
-nocovariates.estimate_betas <- function(data){
+nocovariates.estimate_var_betas <- function(data, var, rho, betas){
+  dtrs <- c("1,1", "1,-1", "-1,1", "-1,-1")
+  dtrenc <- list("1,1" = c(1,1), "1,-1"=c(1,-1), "-1,1"=c(-1,1), "-1,-1"=c(-1,-1))
+  J_hat <- matrix(0, 4, 4)
+  N <- max(data$i)
+  for (d in dtrs){
+    a1 <- dtrenc[[d]][1]
+    a2 <- dtrenc[[d]][2]
+    temp <- matrix(0, 4, 4)
+    for (ci in 1:N){
+      data_sub <- data[data$i == ci, ]
+      ni <- nrow(data_sub)
+      A1 <- data_sub$A1[1]
+      R <- data_sub$R[1]
+      A2 <- data_sub$A2[1]
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
+        temp <- temp + (IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% D(ni, a1, a2))
+      }
+    }
+    J_hat <- J_hat+temp
+  }
+  J_hat <-  J_hat/N
+
+  A_hat <- matrix(0, 4, 4)
+  for (d in dtrs){
+    temp <- matrix(0, 4, 4)
+    a1 <- dtrenc[[d]][1]
+    a2 <- dtrenc[[d]][2]
+    for (ci in 1:N){
+      data_sub <- data[data$i == ci, ]
+      A1 <- data_sub$A1[1]
+      R <- data_sub$R[1]
+      A2 <- data_sub$A2[1]
+      ni <- nrow(data_sub)
+      Y <- matrix(data_sub$Y)
+      diff <- Y - mmm(Y, betas, a1, a2)
+      V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
+      if (IWi(A1, R, A2, a1, a2) != 0){
+        Ui <- IWi(A1, R, A2, a1, a2) * t(D(ni, a1, a2)) %*% solve(V) %*% diff
+        temp <- temp + (Ui %*% t(Ui))
+      }
+    }
+    A_hat <- A_hat + temp
+  }
+  A_hat <- A_hat/N
+
+  var_hat_beta_hat <- (1/N) * (solve(J_hat) %*% A_hat %*% solve(J_hat))
+  return(var_hat_beta_hat)
+}
+
+#' @title Function to get treatment recipe object from the conditional parameters
+#'
+#' @description Returns the treatment recipe object from the conditional parameters
+#' (mean, variance, correlation), number of clusters, number of participants in each cluster,
+#' response probabilities of the different first level treatment options.
+#' conditional mean, variance, and correlation must be numerical lists of length 6 with each
+#' element corresponding to the cells (1, 1, .), (1, 0, 1), (1, 0, -1), (-1, 1, .), (-1, 0, 1), and (-1, 0, -1)
+#' encoded in (A1, R, A2) form.
+#' @param cell_mu a vector of conditional means.
+#' @param cell_var a vector of conditional variances.
+#' @param cell_cor a vector of conditional correlations.
+#' @param N number of clusters to be generated.
+#' @param m number of participants in a cluster.
+#' This can be a numeric variable \code{m_1} in which case, it is assumed that the
+#' number of participants in each cluster is \code{m_1}. This can also be a list of length N
+#' where each element in the list corresponds to the number of participants in the cluster.
+#' @param p_1 probability of response of the first level treatment A_1 = 1
+#' @param p_2 probability of response of the first level treatment A_1 = -1
+#' @return a treatment recipe object containing all the conditional and marginal parameters.
+#' @export nocovairiates.from_conditional
+nocovairiates.from_conditional <- function(cell_mu = c(10,8,2,4,-2,3), cell_var = c(100,100,100,100,100,100),
+                                           cell_cor = c(0.1, 0.2, 0.15, 0.12, 0.11, 0.07), N=200, m=5, p_1=0.3, p_2=0.2){
+
+  if (length(m) != N){
+    m <- rep(m[1], N)
+  }
+
+  p_A1 <- 0.5
+  p_A2 <- 0.5
+
+  q_1 <-  1 - p_1
+  q_2 <-  1 - p_2
+
+  cell_cov <-  cell_var*cell_cor
+
+  treat_mu <-  c((cell_mu[1]*p_1) + (cell_mu[2]*q_1), (cell_mu[1]*p_1) + (cell_mu[3]*q_1),
+                 (cell_mu[4]*p_2) + (cell_mu[5]*q_2), (cell_mu[4]*p_2) + (cell_mu[6]*q_2))
+
+  treat_var <-  c((cell_var[1]*p_1 + cell_var[2]*q_1), (cell_var[1]*p_1 + cell_var[3]*q_1),
+                (cell_var[4]*p_2 + cell_var[5]*q_2), (cell_var[4]*p_2 + cell_var[6]*q_2))
+
+  mean_diff_term <-  c(((cell_mu[1]-cell_mu[2])^2)*p_1*q_1, ((cell_mu[1]-cell_mu[3])^2)*p_1*q_1,
+                     ((cell_mu[4]-cell_mu[5])^2)*p_2*q_2, ((cell_mu[4]-cell_mu[6])^2)*p_2*q_2)
+
+  treat_var <-  treat_var + mean_diff_term
+
+  treat_cov <-  c((cell_cov[1]*p_1) + (cell_cov[2]*q_1), (cell_cov[1]*p_1) + (cell_cov[3]*q_1),
+                (cell_cov[4]*p_2) + (cell_cov[5]*q_2), (cell_cov[4]*p_2 + cell_cov[6]*q_2))
+
+  treat_cov <-  treat_cov + mean_diff_term
+  treat_cor <-  treat_cov/treat_var
+  A <-  matrix(c(1,1,1,1,1,1,-1,-1,1,-1,1,-1,1,-1,-1,1), 4, 4)
+  B <-  matrix(treat_mu, 4, 1)
+  betas <- solve(A, B)
+
+  treat_summary <-  data.frame(treat_mu, treat_var, treat_cor, betas)
+  row.names(treat_summary) <-  c("(1, 1)", "(1, -1)", "(-1, 1)", "(-1,-1)" )
+
+  recipe <- list(cell_mu=cell_mu, cell_var=cell_var, cell_cor=cell_cor, cell_cov=cell_cov,
+                 treat_mu=treat_mu, treat_var=treat_var, treat_cov=treat_cov, treat_cor=treat_cor,
+                 p_1=p_1, p_2=p_2, p_A1=p_A1, p_A2=p_A2, N=N, m=m, betas=betas, treat_summary=treat_summary)
+  return(recipe)
+}
+
+#' @title Function to generate data from the treatment recipe object
+#' @description Generates data from the treatment recipe object. This function can be
+#' reused with the same treatment recipe object to generate more samples with the same parameters.
+#' @param recipe the generated recipe from nocovairiates.from_conditional() function
+#' @return a data.frame object of the generated data with cluster ID, participant ID, A1, R, A2, and response
+#' @export nocovariates.treat
+nocovariates.treat <- function(recipe){
+  encoder <- list("1,1,."=1, "1,0,1"=2, "1,0,-1"=3, "-1,1,."=4, "-1,0,1"=5, "-1,0,-1"=6)
+  N <- recipe$N
+  m_vec <- recipe$m
+  cell_mu <- recipe$cell_mu
+  cell_var <- recipe$cell_var
+  cell_cor <- recipe$cell_cor
+  cell_cov <- recipe$cell_cov
+  p_1 <- recipe$p_1
+  p_2 <- recipe$p_2
+  p_A1 <- recipe$p_A1
+  p_A2 <- recipe$p_A2
+
+  data <- matrix(nrow=0, ncol=6)
+
+  for (clusID in 1:N){
+    m <- m_vec[clusID]
+    A1 <-  (2*rbinom(1, 1, p_A1)) - 1
+
+    if (A1 == 1){
+      R <-  rbinom(1,1,p_1)
+      if (R == 1){
+        treat_code <- "1,1,."
+        A2 <- 1
+      } else if (R == 0){
+        A2 <-  (2*rbinom(1, 1, p_A2)) - 1
+        treat_code <- sprintf("1,0,%d", A2)
+      }
+    } else if (A1 == -1){
+      R <-  rbinom(1, 1 ,p_1)
+      if (R == 1){
+        treat_code <- "-1,1,."
+        A2 <- 1
+      }
+      else if (R == 0){
+        A2 <-  (2*rbinom(1, 1, p_A2)) - 1
+        treat_code <- sprintf("-1,0,%d", A2)
+      }
+    }
+
+    cell_index <- as.numeric(encoder[treat_code])
+    V <-  cell_var[cell_index] * (diag(m)+(cell_cor[cell_index]*(matrix(1,m,m)-diag(m))))
+    Y <- t(cell_mu[cell_index]+mvtnorm::rmvnorm(1, rep(0,m), V, method='chol'))
+
+    tempmat <- cbind(rep(clusID, m), seq(1,m), rep(A1, m), rep(R, m), rep(A2, m), Y)
+    data <- rbind(data, tempmat)
+  }
+  full_data_frame = as.data.frame(data)
+  names(full_data_frame) = c("i", "j", "A1", "R", "A2", "Y")
+  return(full_data_frame)
+}
+
+#' @title Function to estimate beta, variance of beta_hat, and ICC
+#' @param data a data.frame object containing cluster ID, participant ID, A1, R, A2, Y with
+#' column names being i, j, A1, R, A2, Y.
+#' @return a list object containing all the estimated parameters.
+#' @export nocovariates.estimate
+nocovariates.estimate <- function(data){
   var <- list("1,1" = 1, "1,-1"=1, "-1,1"=1, "-1,-1"=1)
   rho <- list("1,1" = 0, "1,-1"=0, "-1,1"=0, "-1,-1"=0)
   betas <- get_beta(var, rho, data)
@@ -263,7 +336,7 @@ nocovariates.estimate_betas <- function(data){
   varrho_stars <- get_varrho_star(data, epsilon)
   var <- varrho_stars$var_star
   rho <- varrho_stars$rho_star
-  return(list(beta_hat=betas, var_hat=var, rho_hat=rho))
+
+  cov_hat <- nocovariates.estimate_var_betas(data, var, rho, betas)
+  return(list(beta_hat=betas, var_hat=var, rho_hat=rho, cov_hat_beta_hat=cov_hat))
 }
-
-
--- R/treatment_summary.R
+++ R/treatment_summary.R
@@ -1,5 +1,6 @@
 #' @title Function to get treatment summary from the data generator
 #' @param recipe the generated recipe from nocovairiates.from_conditional()
+#' @return a data.frame object containing the true marginal parameters and true betas
 #' @export treatment_summary
 
 treatment_summary <- function(recipe){
