2c38eaf831b912519d9acc13aa96cd26e8332205
Anil Battalahalli <anilbattalahalli@automata-2.local>
2022-06-15 14:55:49 +0530

    Modified functions to accept custom cluster-level weights

--- R/clustered.R
+++ R/clustered.R
@@ -6,6 +6,14 @@
 #' @importFrom pbmcapply pbmclapply
 NULL
 
+Ii <- function(A1, R, A2, a1, a2){
+  ifelse(A1==a1,1,0)*(R+(ifelse(A2==a2,1,0)*(1-R)))
+}
+
+Wi <- function(R){
+  2*(R+(2*(1-R)))
+}
+
 IWi <- function(A1, R, A2, a1, a2){
   2*ifelse(A1==a1,1,0)*(R+(2*ifelse(A2==a2,1,0)*(1-R)))
 }
@@ -34,13 +42,13 @@ mmm <- function(designmat, betas){
 get_residuals <- function(betas, data, formula){
   dtrs <- c("1,1", "1,-1", "-1,1", "-1,-1")
   dtrenc <- list("1,1" = c(1,1), "1,-1"=c(1,-1), "-1,1"=c(-1,1), "-1,-1"=c(-1,-1))
-  N <- max(data$i)
+  N <- data$i %>% unique %>% length
   epsilon <- list()
   for (d in dtrs){
     a1 <- dtrenc[[d]][1]
     a2 <- dtrenc[[d]][2]
     temp <- list()
-    for (ci in 1:N){
+    for (ci in unique(data$i)){
       data_sub <- data[data$i == ci, ]
       temp[ci] <- list(as.numeric(data_sub$Y - mmm(D(data_sub, a1, a2, formula), betas)))
     }
@@ -49,28 +57,33 @@ get_residuals <- function(betas, data, formula){
   return(epsilon)
 }
 
-get_varrho_star <- function(data, epsilon){
+get_varrho_star <- function(data, epsilon, weights){
   dtrs <- c("1,1", "1,-1", "-1,1", "-1,-1")
   dtrenc <- list("1,1" = c(1,1), "1,-1"=c(1,-1), "-1,1"=c(-1,1), "-1,-1"=c(-1,-1))
   var_star <- list()
   rho_star <- list()
-  N <- max(data$i)
+  N <- data$i %>% unique %>% length
 
   for (d in dtrs){
     a1 <- dtrenc[[d]][1]
     a2 <- dtrenc[[d]][2]
     s <- 0
     den <- 0
-    for (ci in 1:N){
+    for (ci in unique(data$i)){
       data_sub <- data[data$i == ci, ]
       ni <- nrow(data_sub)
       A1 <- data_sub$A1[1]
       R <- data_sub$R[1]
       A2 <- data_sub$A2[1]
       eps_sq <- epsilon[[d]][[ci]]^2
-      if (IWi(A1, R, A2, a1, a2) != 0){
-        s <- s + (IWi(A1, R, A2, a1, a2) * sum(eps_sq))
-        den <- den + (IWi(A1, R, A2, a1, a2) * ni)
+      if (is.null(weights) == F){
+        W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+      } else{
+        W = IWi(A1, R, A2, a1, a2)
+      }
+      if (W != 0){
+        s <- s + (W * sum(eps_sq))
+        den <- den + (W * ni)
       }
     }
     var_star[d] <- s/den
@@ -81,7 +94,7 @@ get_varrho_star <- function(data, epsilon){
     a2 <- dtrenc[[d]][2]
     s <- 0
     den <- 0
-    for (ci in 1:N){
+    for (ci in unique(data$i)){
       data_sub <- data[data$i == ci, ]
       ni <- nrow(data_sub)
       if (ni > 1){
@@ -89,9 +102,14 @@ get_varrho_star <- function(data, epsilon){
         R <- data_sub$R[1]
         A2 <- data_sub$A2[1]
         eps_mul_sum <- perm_mul_sum(epsilon[[d]][[ci]])
-        if (IWi(A1, R, A2, a1, a2) != 0){
-          s <- s + (IWi(A1, R, A2, a1, a2) * eps_mul_sum)
-          den <- den + (IWi(A1, R, A2, a1, a2) * ni * (ni - 1))
+        if (is.null(weights) == F){
+          W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+        } else{
+          W = IWi(A1, R, A2, a1, a2)
+        }
+        if (W != 0){
+          s <- s + (W * eps_mul_sum)
+          den <- den + (W * ni * (ni - 1))
         }
       }
     }
@@ -102,13 +120,13 @@ get_varrho_star <- function(data, epsilon){
   return(rhovar_stars)
 }
 
-get_beta <- function(var, rho, data, formula){
+get_beta <- function(var, rho, data, formula, weights){
   params <- (formula %>% terms %>% labels %>% length)+1
   dtrs <- c("1,1", "1,-1", "-1,1", "-1,-1")
   dtrenc <- list("1,1" = c(1,1), "1,-1"=c(1,-1), "-1,1"=c(-1,1), "-1,-1"=c(-1,-1))
   f <- matrix(0, params, params)
-  N <- max(data$i)
-  for (ci in 1:N){
+  N <- data$i %>% unique %>% length
+  for (ci in unique(data$i)){
     data_sub <- data[data$i == ci, ]
     A1 <- data_sub$A1[1]
     R <- data_sub$R[1]
@@ -117,15 +135,20 @@ get_beta <- function(var, rho, data, formula){
     for (d in dtrs){
       a1 <- dtrenc[[d]][1]
       a2 <- dtrenc[[d]][2]
-      if (IWi(A1, R, A2, a1, a2) != 0){
+      if (is.null(weights) == F){
+        W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+      } else{
+        W = IWi(A1, R, A2, a1, a2)
+      }
+      if (W != 0){
         V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
-        f = f + (IWi(A1, R, A2, a1, a2) * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% D(data_sub, a1, a2, formula))
+        f = f + (W * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% D(data_sub, a1, a2, formula))
       }
     }
   }
 
   s <- matrix(0, params, 1)
-  for (ci in 1:N){
+  for (ci in unique(data$i)){
     data_sub <- data[data$i == ci, ]
     A1 <- data_sub$A1[1]
     R <- data_sub$R[1]
@@ -135,7 +158,12 @@ get_beta <- function(var, rho, data, formula){
     for (d in dtrs){
       a1 <- dtrenc[[d]][1]
       a2 <- dtrenc[[d]][2]
-      if (IWi(A1, R, A2, a1, a2) != 0){
+      if (is.null(weights) == F){
+        W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+      } else{
+        W = IWi(A1, R, A2, a1, a2)
+      }
+      if (W != 0){
         V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
         s <- s + (IWi(A1, R, A2, a1, a2) * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% Y)
       }
@@ -145,25 +173,30 @@ get_beta <- function(var, rho, data, formula){
   return(beta_hat)
 }
 
-clustered.estimate_var_betas <- function(data, var, rho, betas, formula){
+clustered.estimate_var_betas <- function(data, var, rho, betas, formula, weights){
   params <- (formula %>% terms %>% labels %>% length)+1
   dtrs <- c("1,1", "1,-1", "-1,1", "-1,-1")
   dtrenc <- list("1,1" = c(1,1), "1,-1"=c(1,-1), "-1,1"=c(-1,1), "-1,-1"=c(-1,-1))
   J_hat <- matrix(0, params, params)
-  N <- max(data$i)
+  N <- data$i %>% unique %>% length
   for (d in dtrs){
     a1 <- dtrenc[[d]][1]
     a2 <- dtrenc[[d]][2]
     temp <- matrix(0, params, params)
-    for (ci in 1:N){
+    for (ci in unique(data$i)){
       data_sub <- data[data$i == ci, ]
       ni <- nrow(data_sub)
       A1 <- data_sub$A1[1]
       R <- data_sub$R[1]
       A2 <- data_sub$A2[1]
-      if (IWi(A1, R, A2, a1, a2) != 0){
+      if (is.null(weights) == F){
+        W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+      } else{
+        W = IWi(A1, R, A2, a1, a2)
+      }
+      if (W != 0){
         V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
-        temp <- temp + (IWi(A1, R, A2, a1, a2) * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% D(data_sub, a1, a2, formula))
+        temp <- temp + (W * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% D(data_sub, a1, a2, formula))
       }
     }
     J_hat <- J_hat+temp
@@ -175,7 +208,7 @@ clustered.estimate_var_betas <- function(data, var, rho, betas, formula){
     temp <- matrix(0, params, params)
     a1 <- dtrenc[[d]][1]
     a2 <- dtrenc[[d]][2]
-    for (ci in 1:N){
+    for (ci in unique(data$i)){
       data_sub <- data[data$i == ci, ]
       A1 <- data_sub$A1[1]
       R <- data_sub$R[1]
@@ -184,8 +217,13 @@ clustered.estimate_var_betas <- function(data, var, rho, betas, formula){
       Y <- matrix(data_sub$Y)
       diff <- Y - mmm(D(data_sub, a1, a2, formula), betas)
       V = var[[d]] * (diag(ni)+(rho[[d]]*(matrix(1,ni,ni)-diag(ni))))
-      if (IWi(A1, R, A2, a1, a2) != 0){
-        Ui <- IWi(A1, R, A2, a1, a2) * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% diff
+      if (is.null(weights) == F){
+        W = weights[weights$i == ci,]$W[1] * Ii(A1, R, A2, a1, a2)
+      } else{
+        W = IWi(A1, R, A2, a1, a2)
+      }
+      if (W != 0){
+        Ui <- W * t(D(data_sub, a1, a2, formula)) %*% solve(V) %*% diff
         temp <- temp + (Ui %*% t(Ui))
       }
     }
@@ -197,41 +235,59 @@ clustered.estimate_var_betas <- function(data, var, rho, betas, formula){
   return(var_hat_beta_hat)
 }
 
+#' @title Function to prepare data for estimation
+#' @param data data frame to be prepared for estimation
+#' @param i cluster ID column name
+#' @param A1 A1 column name
+#' @param R R column name
+#' @param A2 A2 column name
+#' @param Y outcome variable column name
+#' @return a data frame ready to be plugging into the estimation function
+#' @export clustered.prepare_data
+clustered.prepare_data <- function(data, i='i', A1='A1', R='R', A2='A2', Y='Y'){
+  names(data)[names(data) == i] <- 'i'
+  names(data)[names(data) == A1] <- 'A1'
+  names(data)[names(data) == R] <- 'R'
+  names(data)[names(data) == A2] <- 'A2'
+  names(data)[names(data) == Y] <- 'Y'
+  return(data)
+}
+
 #' @title Function to estimate beta, variance of beta_hat, and ICC
 #' @param formula formula for the estimation
 #' @param data a data.frame object containing cluster ID, participant ID, A1, R, A2, Y with
 #' column names being i, j, A1, R, A2, Y
 #' @param est_var T/F to estimate the variance or not
+#' @param weights a data frame containing cluster level weights with columns `i` and `W` for cluster ID and weights respectively
 #' @return estimates object containing all the estimated parameters.
 #' @export clustered.estimate
-clustered.estimate <- function(formula, data, est_var = T){
+clustered.estimate <- function(formula, data, est_var = T, weights=NULL){
   var <- list("1,1" = 1, "1,-1"=1, "-1,1"=1, "-1,-1"=1)
   rho <- list("1,1" = 0, "1,-1"=0, "-1,1"=0, "-1,-1"=0)
 
-  betas <- get_beta(var, rho, data, formula)
+  betas <- get_beta(var, rho, data, formula, weights)
   epsilon <- get_residuals(betas, data, formula)
-  varrho_stars <- get_varrho_star(data, epsilon)
+  varrho_stars <- get_varrho_star(data, epsilon, weights)
   var <- varrho_stars$var_star
   rho <- varrho_stars$rho_star
 
-  betas <- get_beta(var, rho, data, formula)
+  betas <- get_beta(var, rho, data, formula, weights)
   epsilon <- get_residuals(betas, data, formula)
-  varrho_stars <- get_varrho_star(data, epsilon)
+  varrho_stars <- get_varrho_star(data, epsilon, weights)
   var <- varrho_stars$var_star
   rho <- varrho_stars$rho_star
 
-  betas <- get_beta(var, rho, data, formula)
+  betas <- get_beta(var, rho, data, formula, weights)
   epsilon <- get_residuals(betas, data, formula)
-  varrho_stars <- get_varrho_star(data, epsilon)
+  varrho_stars <- get_varrho_star(data, epsilon, weights)
   var <- varrho_stars$var_star
   rho <- varrho_stars$rho_star
 
   if (est_var){
-    cov_hat <- clustered.estimate_var_betas(data, var, rho, betas, formula)
+    cov_hat <- clustered.estimate_var_betas(data, var, rho, betas, formula, weights)
     return(list(beta_hat=betas, var_hat=var, rho_hat=rho, cov_hat_beta_hat=cov_hat, formula=formula))
   } else{
     return(list(beta_hat=betas, var_hat=var, rho_hat=rho, formula=formula))
   }
 
 }
-
